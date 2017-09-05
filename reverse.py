b3ea7c738fd7d6de5ccdc63bbffbcb5bd0bd373f
arshad0110 <31645327+arshad0110@users.noreply.github.com>
2017-09-05 06:29:18 +0000

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,19 @@
+list2=[]
+
+def rec(list1):
+    '''  
+    Objective : to reverse the elements of the list
+    input parameters:
+        list1: list to be reversed
+    return values : 
+        list2 : reversed list
+             
+    '''
+    #Approach : computing using length of the list
+    
+    length=len(list1)
+    if list1==[]:
+        return list2
+    else:
+        list2.append(list1[length-1])
+        return rec(list1[0:length-1])
