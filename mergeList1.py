e933d3e1613de68802e4e4b3cd53db9a2a5a7962
arshad0110 <31645327+arshad0110@users.noreply.github.com>
2017-11-03 12:21:51 +0530

    Update mergeList1.py

--- mergeList1.py
+++ mergeList1.py
@@ -42,7 +42,7 @@ def sortMergeList( List1, List2,List3=[] ,in1=0, in2=0 ):
         else :
 	    List3 = List3 + List1[in1:]
 	    return List3
-	elif(List1[in1]==List2[in2]):
+    elif(List1[in1]==List2[in2]):
 	    List3.append(List1[in1])
 	    List3 = List3 + List1[in2]
 	
